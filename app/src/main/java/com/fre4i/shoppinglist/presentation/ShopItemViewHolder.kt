6dc781a2d743e2687d3ea788eb4006e1fb4fc216
Vasyan <vasyan.coder@gmail.com>
2023-02-09 21:47:59 +0300

    feat: implement data binding in item_shop

--- app/src/main/java/com/fre4i/shoppinglist/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/com/fre4i/shoppinglist/presentation/ShopItemViewHolder.kt
@@ -2,10 +2,11 @@ package com.fre4i.shoppinglist.presentation
 
 import android.view.View
 import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.fre4i.shoppinglist.R
+import com.fre4i.shoppinglist.databinding.ItemShopDisabledBinding
 
-class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    val tvName = view.findViewById<TextView>(R.id.tv_name)
-    val tvCount = view.findViewById<TextView>(R.id.tv_count)
-}
+class ShopItemViewHolder(
+    val binding: ViewDataBinding
+) : RecyclerView.ViewHolder(binding.root)
